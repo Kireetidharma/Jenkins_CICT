646cde373446b237f98074b9bc49be0fad0bddcf
Dharma Kireeti <45656067+Kireetidharma@users.noreply.github.com>
2022-11-13 15:47:17 +0530

    Update test_hello.sh

--- test_hello.sh
+++ test_hello.sh
@@ -1,1 +1,1 @@
-[[ `./hello.sh` == *"Hello"* ]] && (echo "Hurrah!! our test passed! as the file contains Hello"; exit 0) || (echo "test failed :-( - not able to find Hello hence pipeline failed"; exit 1)
+[[ `./hello.sh` == *"Hello"* ]] && (echo "Hurrah!! our test passed! as the file contains 'Hello'"; exit 0) || (echo "test failed :-( - not able to find 'Hello', hence pipeline failed"; exit 1)
